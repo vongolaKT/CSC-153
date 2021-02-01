1c1f3d0c1e85fb9a2ccb44e387a4b98c8237e552
Kevin Griffin <kevin.baseball97@yahoo.com>
2021-01-31 23:55:19 -0500

    It fills the requirements but button does nothing even though moving code to button was only way to get it to run........ idk its frustrating....

---
+++ TotalSalesHardcoded/TotalSales/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace TotalSales
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // ARR size with const variable
+        const int SIZE = 7;
+
+        // Hardcoded these values but they can be input variables
+        double[] salesArr = new double[SIZE]
+
+           {1245.67, 1189.55, 1098.72, 1456.88, 2109.34, 1987.55, 1872.36};
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        { 
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // loop that displays the values in the array to a list box
+            for (int index = 0; index < salesArr.Length; index++)
+            {
+                listBox1.Items.Add(salesArr[index].ToString());
+            }
+
+            // create accumulator variable
+            double totalSales = 0;
+
+            // adds each value in the array to accumulator variable in each loop
+            for (int index = 0; index < salesArr.Length; index++)
+            {
+                totalSales += salesArr[index];
+            }
+
+            // Display the total of all values or totalSales
+            MessageBox.Show("The total amount of sales are $" + totalSales.ToString());
+        }
+    }
+}
