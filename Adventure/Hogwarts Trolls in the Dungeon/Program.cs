a7bf081bd3038cc3404896915575f4942e9fb4fd
Kevin Griffin <kevin.baseball97@yahoo.com>
2021-02-07 01:05:03 -0500

    Added all arrays and lists, also imprived visibility on output lines

--- Adventure/Hogwarts Trolls in the Dungeon/Program.cs
+++ Adventure/Hogwarts Trolls in the Dungeon/Program.cs
@@ -23,19 +23,28 @@ namespace Hogwarts_Trolls_in_the_Dungeon
             // Arrays and Lists
 
             
-            string [] Rooms = new string[] {"Hogwart's Express", "Lake", "Entrance Hall", "Dungeons", "Common Room"}; // TODO Make rooms objects? and add more rooms with ability to go west and east.
+            string[] Rooms = new string[] {"Hogwart's Express", "Lake", "Entrance Hall", "Dungeons", "Common Room"}; // TODO Make rooms objects? and add more rooms with ability to go west and east.
 
-            string [] Spells = new string[] { "Stupefy", "Augmenti", "Incendio", "Incarcerous"}; // TODO add spells they also need to be objects
+            string[] Spells = new string[] { "Stupefy", "Augmenti", "Incendio", "Incarcerous"}; // TODO add spells they also need to be objects
 
+            string[] Potions = new string[] { "Damage", "Sheild" }; // TODO also objects, need a counter for how many in inventory also
 
+            string[] Money = new string[] { "Galleon(s)","Sickle(s)", "Knut(s)" }; // objects also need counter to keep track will have stores to buy things in.
 
+            List<string> Items = new List<string>();
+            Items.Add("Gillyweed");
+            Items.Add("Broomstick");
+            Items.Add("Deluminator");
+            Items.Add("Elder Wand");
 
+            List<string> Enemies = new List<string>();
+            Enemies.Add("Kappa"); // Water
+            Enemies.Add("Blast-Ended Skrewt"); // Fire
+            Enemies.Add("Dugbog"); // Grass/Wood
+            Enemies.Add("Nogtail");// Neutral
+            Enemies.Add("Mountain Troll"); // Boss - Neutral
 
-
-
-
-
-            // Some usefull variables
+            // Some useful variables
 
             int playerPos = 0;
             
@@ -46,6 +55,7 @@ namespace Hogwarts_Trolls_in_the_Dungeon
 
             while (exit == false)
             {
+                Console.ForegroundColor = ConsoleColor.White;
                 Console.WriteLine("________________________________________________________________________________________________________________________");
                 Console.WriteLine("Enter number or \"n\" to move North or \"s\" to move South");
                 Console.WriteLine("1. Rooms");
@@ -59,9 +69,12 @@ namespace Hogwarts_Trolls_in_the_Dungeon
                 Console.Write("Enter a choice > ");
                 string input = Console.ReadLine();
 
+                Console.ForegroundColor = ConsoleColor.DarkCyan;                    // Change color of output lines makes it easier to read from menu
+
                 switch (input)
                 {
                     case "1":
+
                         foreach (string index in Rooms)
                         {
                             Console.WriteLine(index);
@@ -78,19 +91,31 @@ namespace Hogwarts_Trolls_in_the_Dungeon
                         break;
 
                     case "3":
-                        // TODO Potions
+                        foreach (string index in Potions)
+                        {
+                            Console.WriteLine(index);                   // TODO add line that displays count
+                        }
                         break;
 
                     case "4":
-                        // TODO Money
+                        foreach (string index in Money)
+                        {
+                            Console.WriteLine(index);                   // TODO add line that displays count
+                        }
                         break;
 
                     case "5":
-                        // TODO Items
+                        foreach (string index in Items)
+                        {
+                            Console.WriteLine(index);
+                        }
                         break;
 
                     case "6":
-                        // TODO Enemies
+                        foreach (string index in Enemies)
+                        {
+                            Console.WriteLine(index);
+                        }
                         break;
 
                     case "7":
