6e4039a13739c7057a66f1a82488a3db48806a4c
Kevin Griffin <kevin.baseball97@yahoo.com>
2021-02-21 23:09:42 -0500

    Finished, maybe could add input validation to make sure input is a double and if not displays an error instead of crashing.

--- FallingDistance/WinFormUI/Form1.cs
+++ FallingDistance/WinFormUI/Form1.cs
@@ -8,6 +8,14 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+/**
+* 02/21/2021
+* CSC 153
+* Kevin Griffin
+* Program that takes a given amount of time and ehen executed displays how far an object would have travelled (at terminal velocity) in meters.
+* From page 382 in textbook
+*/
+
 namespace WinFormUI
 {
     public partial class Form1 : Form
@@ -28,6 +36,21 @@ namespace WinFormUI
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+          
+            double time = Double.Parse(textBox1.Text);
+
+            double answer = FallingDistance(time);
+
+            textBox2.Text = answer.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
